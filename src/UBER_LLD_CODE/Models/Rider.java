c834a7fbf1cbd328876928411dfa130f779b7fd6
HP <sobaasvini2410@gmail.com>
2025-07-10 13:21:02 +0530

    "rider commit"

---
+++ src/UBER_LLD_CODE/Models/Rider.java
@@ -0,0 +1,14 @@
+package UBER_LLD_CODE.Models;
+
+public class Rider {
+    private String name;
+
+    public Rider(String name){
+        this.name=name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
+
