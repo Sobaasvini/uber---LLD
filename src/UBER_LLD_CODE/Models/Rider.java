7c911115d2c5fe163cd3e5f7ad0cc23e1497a383
HP <sobaasvini2410@gmail.com>
2025-07-10 14:06:56 +0530

    rating

--- src/UBER_LLD_CODE/Models/Rider.java
+++ src/UBER_LLD_CODE/Models/Rider.java
@@ -2,13 +2,18 @@ package UBER_LLD_CODE.Models;
 
 public class Rider {
     private String name;
+    private int rating;
 
-    public Rider(String name){
+    public Rider(String name,int rating){
         this.name=name;
+        this.rating=rating;
     }
 
     public String getName(){
         return name;
+    }
+    public int getRating(){
+        return rating;
     }
 }
 
